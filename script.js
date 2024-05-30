24558547c07ae30cce356195144edc1ebbbbbdbf
aditya rahman <aditya.rahman@bfi.co.id>
2024-05-30 16:06:50 +0700

    menambahkan lokasi cuaca pada script.js dengan funtion fetchData dan bisa di lihat pada terminaldengan menambahkan searchCity dan apiKey

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-document.getElementById("search-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const city = document.getElementById("city-input").value;
-    fetchWeather(city);
-});
+async function fetchData() {
+    let searchCity = 'garut'
+    let apiKey = `e626ac138781a789351e65806e5c22ea`
+    const responseOpenWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`)
+    const responseOpenWeatherJson = await responseOpenWeather.json()
+    console.log(responseOpenWeatherJson, "<< response Open Weather Json")
 
-function fetchWeather(city) {
-    const apiKey = "e626ac138781a789351e65806e5c22ea";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ID}&appid=${e626ac138781a789351e65806e5c22ea}&units=metric`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const weatherDescription = data.weather[0].description;
-            const temperature = data.main.temp;
-            const weatherInfo = `Cuaca: ${weatherDescription}, Suhu: ${temperature}°C`;
-            document.getElementById("weather-info").textContent = weatherInfo;
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            document.getElementById("weather-info").textContent = 'Gagal memuat informasi cuaca';
-        });
+    let lat = responseOpenWeatherJson.coord.lat;
+    let lon = responseOpenWeatherJson.coord.lon;
+    
+    const responseOpenMeteo = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FBangkok`)
+    const responseOpenMeteoJson = await responseOpenMeteo.json()
+    console.log(responseOpenMeteoJson, "<< response Open Meteo Json")
 }
+
+fetchData()
